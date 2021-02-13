407c521561794cfac1b203d678db5cb06df63cd7
Eugene Grushin <38574843+euggrush@users.noreply.github.com>
2021-02-12 21:31:55 -0800

    add

---
+++ src/components/films/id/Film.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Film = () => {
+  return <React.Fragment>
+    <section className="movie-card movie-card--full">
+      <div className="movie-card__hero">
+
+      </div>
+    </section>
+  </React.Fragment>;
+};
