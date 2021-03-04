2f33ddaba185211ba3636dc81ca151fbf70f48cf
euggrush <euggrush@gmail.com>
2021-03-03 22:36:52 -0800

    add

---
+++ src/components/video-player/video-player.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VideoPlayer = () => {
+  return (
+    <React.Fragment>
+      <div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default VideoPlayer;
